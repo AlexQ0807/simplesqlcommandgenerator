c1a59ce12824e84dde626a619f2ed22cd8a4a5ad
Alex Q <alex.quan0807@gmail.com>
2023-01-03 18:09:58 -0500

    updated patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='simplesqlcommandgenerator',
-    version='0.0.3',
+    version='0.0.4',
     author='Alex Q',
     author_email='alex.quan0807@gmail.com',
     description='Package for generating basic SQL command',
